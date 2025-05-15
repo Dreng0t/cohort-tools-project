3dae5d03170940c90d8f7196fb11881d9ddb0319
Midas van Soest <midasvsoest@gmail.com>
2025-05-15 14:16:14 +0200

    removed comment in cohort routes

--- server/routes/cohort.routes.js
+++ server/routes/cohort.routes.js
@@ -6,19 +6,6 @@ const Cook = require("../models/Cook.model");
 //
 // POST /cooks
 //
-/* router.post("/cooks", function (req, res, next) {
-    const newCook = req.body;
-
-    Cook.create(newCook)
-        .then((cookFromDB) => {
-            res.status(201).json(cookFromDB)
-        })
-        .catch(error => {
-            console.log("Error creating a new cook in the DB...");
-            console.log(error);
-            res.status(500).json({ error: "Failed to create a new cook" });
-        });
-}) */
 
 router.get("/api/cohorts", (req, res, next) => {
   Cohort.find({})
