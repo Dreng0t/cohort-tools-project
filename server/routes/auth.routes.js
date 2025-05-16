afcfe01bd479b513713ff62ba0637ca7be38341e
Dreng0t <louischicot4@gmail.com>
2025-05-16 15:59:55 +0100

    fixed the issue with the route to user profile, user.model was named incorrectly

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
